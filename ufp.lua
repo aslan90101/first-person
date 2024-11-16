7d67004e74364ce8e8069d342c11aed21d965221
aslan90101 <aslan0aslanov0ezz@gmail.com>
2024-11-16 15:35:22 +0300

    Update ufp.lua

--- ufp.lua
+++ ufp.lua
@@ -1,1 +1,6 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+game:GetService("Players").LocalPlayer.CameraMaxZoomDistance = 99999
+game:GetService("Players").LocalPlayer.CameraMode = Enum.CameraMode.Classic
 
